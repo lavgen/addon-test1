21978d843000c84f22493903ebc57c230f78119e
lavgen <lavgen@artic.edu>
2016-04-28 14:05:39 -0500

    initial files for add-on

---
+++ fuckGoogle.js
@@ -0,0 +1,11 @@
+
+
+alert("YOUR DATA IS NOT SAFE")
+document.body.style.background = "pink";
+document.addEventListener("mousemove" , function( event ){
+	var s = 0;
+
+	 	document.body.style.background= s;
+
+	console.log(s);
+});
